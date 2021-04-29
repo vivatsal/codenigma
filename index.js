d0e7b78d16af443deab251c940c15f5e29d89df6
vivatsal <78379149+vivatsal@users.noreply.github.com>
2021-04-30 02:20:28 +0530

    Initial upload files

---
+++ index.js
@@ -0,0 +1,2 @@
+document.getElementById("login").addEventListener("click",function(){document.getElementById("form").style.display="block";});
+document.getElementById("close").addEventListener("click",function(){document.getElementById("form").style.display="none";});
